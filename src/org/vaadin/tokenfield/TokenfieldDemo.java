7327b186896d608a90781ef288c35a78a1487406
marcenglund <marcenglund@a0bca8ea-11c7-11df-91e2-2fde445baa4c>
2010-02-17 19:28:31 +0000

    Seems feature complete. Added full featured demo.

--- src/org/vaadin/tokenfield/TokenfieldDemo.java
+++ src/org/vaadin/tokenfield/TokenfieldDemo.java
@@ -1,5 +1,6 @@
 package org.vaadin.tokenfield;
 
+import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
@@ -7,14 +8,16 @@ import org.vaadin.tokenfield.TokenField.InsertPosition;
 
 import com.vaadin.Application;
 import com.vaadin.data.Container;
-import com.vaadin.data.Item;
 import com.vaadin.data.Property.ValueChangeEvent;
 import com.vaadin.data.Property.ValueChangeListener;
-import com.vaadin.data.util.IndexedContainer;
+import com.vaadin.data.util.BeanItem;
+import com.vaadin.data.util.BeanItemContainer;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.CheckBox;
 import com.vaadin.ui.ComboBox;
 import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.Form;
 import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Layout;
@@ -26,9 +29,6 @@ import com.vaadin.ui.Window;
 import com.vaadin.ui.Button.ClickEvent;
 
 public class TokenfieldDemo extends Application {
-    // TODO make more real-life examples:
-    // - email address entry
-    // - tag entry, icon + caption -> icon + tooltip
 
     @Override
     public void init() {
@@ -46,46 +46,86 @@ public class TokenfieldDemo extends Application {
                  * empty to begin with, the user can enter new tokens.
                  */
 
-                Panel p = new Panel("Sizes");
-                addComponent(p);
-
-                TokenField f = new TokenField("100% n");
-                f.setWidth("100%");
-                p.addComponent(f);
-
-                f = new TokenField("100% 100%");
-                f.setWidth("100%");
-                f.setInputWidth("100%");
-                p.addComponent(f);
-
-                f = new TokenField("CSS 100% n");
-                f.setLayout(new CssLayout());
-                f.setWidth("100%");
-                p.addComponent(f);
-
-                f = new TokenField("CSS 100% n");
-                f.setLayout(new CssLayout());
-                f.setWidth("100%");
-                f.setInputWidth("100%");
-                p.addComponent(f);
-
-                f = new TokenField("CSS n 100%");
-                f.setLayout(new CssLayout());
-                f.setInputWidth("100%");
-                p.addComponent(f);
-
-            }
-
-            {
-                /*
-                 * This is the most basic use case using all defaults; it's
-                 * empty to begin with, the user can enter new tokens.
-                 */
-
                 Panel p = new Panel("Basic");
                 addComponent(p);
 
                 TokenField f = new TokenField("Add tags");
+                p.addComponent(f);
+            }
+
+            {
+                /*
+                 * In this example, most features are exercised. A container
+                 * with generated contacts is used. The input has filtering
+                 * (a.k.a suggestions) enabled, and the added token button is
+                 * configured so that it is in the standard "Name <email>"
+                 * -format. New contacts can be added to the container ('address
+                 * book'), or added as-is (in which case it's styled
+                 * differently).
+                 */
+
+                Panel p = new Panel("Full featured");
+                p.getContent().setStyleName("black");
+                addComponent(p);
+
+                // generate container
+                Container tokens = generateTestContainer();
+
+                // we want this to be vertical
+                VerticalLayout lo = new VerticalLayout();
+                lo.setSpacing(true);
+
+                final TokenField f = new TokenField(lo) {
+                    protected void onTokenInput(Object tokenId) {
+                        Set<Object> set = (Set<Object>) getValue();
+                        Contact c = new Contact("", tokenId.toString());
+                        if (set != null && set.contains(c)) {
+                            // duplicate
+                            getWindow().showNotification(
+                                    getTokenCaption(tokenId)
+                                            + " is already added");
+                        } else {
+                            if (!cb.containsId(c)) {
+                                // don't add directly,
+                                // show custom "add to address book" dialog
+                                addWindow(new EditContactWindow(tokenId
+                                        .toString(), this));
+                            } else {
+                                // it's in the 'address book', just add
+                                addToken(tokenId);
+                            }
+                        }
+                    }
+
+                    protected void onTokenClicked(final Object tokenId) {
+                        getWindow().addWindow(
+                                new RemoveWindow((Contact) tokenId, this));
+                    }
+
+                    protected void configureTokenButton(Object tokenId,
+                            Button button) {
+                        super.configureTokenButton(tokenId, button);
+                        // custom caption
+                        button.setCaption(getTokenCaption(tokenId) + " <"
+                                + tokenId + ">");
+                        // width
+                        button.setWidth("100%");
+
+                        if (!cb.containsId(tokenId)) {
+                            // it's not in the address book; style
+                            button
+                                    .addStyleName(TokenField.STYLE_BUTTON_EMPHAZISED);
+                        }
+                    }
+                };
+                f.setStyleName(TokenField.STYLE_TOKENFIELD); // no fake textfield look
+                f.setWidth("100%");
+                f.setInputWidth("100%");
+                f.setContainerDataSource(tokens); // 'address book'
+                f.setFilteringMode(ComboBox.FILTERINGMODE_CONTAINS); // suggest
+                f.setTokenCaptionPropertyId("name"); // use name in input
+                f.setInputPrompt("Enter contact name or new email address");
+                f.setRememberNewTokens(false); // we'll do this via the dialog
                 p.addComponent(f);
             }
 
@@ -163,128 +203,8 @@ public class TokenfieldDemo extends Application {
                     }
                 });
                 controls.addComponent(cb);
+                controls.setComponentAlignment(cb, Alignment.BOTTOM_LEFT);
 
-            }
-
-            {
-                /*
-                 * In this example a container with generated contacts is used.
-                 * The input has filtering (a.k.a suggestions) enabled, and the
-                 * added token button is configured so that it is in the
-                 * standard "Name <email>" -format.
-                 */
-
-                Panel p = new Panel("Customize token button");
-                addComponent(p);
-
-                // generate container
-                Container tokens = generateTestContainer();
-
-                // we want this to be vertical
-                VerticalLayout lo = new VerticalLayout();
-                lo.setSpacing(true);
-
-                final TokenField f = new TokenField(lo) {
-                    protected void configureTokenButton(Object tokenId,
-                            Button button) {
-                        // otherwise default, but change caption
-                        super.configureTokenButton(tokenId, button);
-                        button.setCaption(getTokenCaption(tokenId) + "<"
-                                + tokenId + ">");
-                    }
-                };
-                f.setContainerDataSource(tokens); // datasource
-                f.setNewTokensAllowed(false); // let's not add new emails now
-                f.setFilteringMode(ComboBox.FILTERINGMODE_CONTAINS); // suggest
-                f.setTokenCaptionPropertyId("name"); // use name in input
-                f.setInputPrompt("Contact name");
-                p.addComponent(f);
-            }
-
-            {
-                /*
-                 * User-added tokens are custom styled, not added to container.
-                 */
-
-                Panel p = new Panel("Customize new tokens");
-                addComponent(p);
-
-                // generate container
-                Container tokens = generateTestContainer();
-
-                // we want this to be vertical
-                VerticalLayout lo = new VerticalLayout();
-                lo.setSpacing(true);
-
-                final TokenField f = new TokenField(lo) {
-                    protected void configureTokenButton(Object tokenId,
-                            Button button) {
-                        // otherwise default, but change caption
-                        super.configureTokenButton(tokenId, button);
-                        if (!cb.containsId(tokenId)) {
-                            button.setCaption("(New) " + button.getCaption());
-                            button.addStyleName("emphasize");
-                        }
-                    }
-                };
-                f.setContainerDataSource(tokens); // datasource
-                f.setFilteringMode(ComboBox.FILTERINGMODE_CONTAINS); // suggest
-                f.setRememberNewTokens(false); // but don't add new suggestions
-
-                p.addComponent(f);
-            }
-
-            {
-                /*
-                 * Here, onTokenInput() and onTokenClicked() is customized.
-                 */
-                Panel p = new Panel("Add / remove actions");
-                addComponent(p);
-
-                final TokenField f = new TokenField() {
-
-                    protected void onTokenInput(Object tokenId) {
-                        Set<Object> set = (Set<Object>) getValue();
-                        if (set != null && set.contains(tokenId)) {
-                            getWindow().showNotification("Duplicate");
-                        } else {
-                            getWindow().showNotification("Added");
-                            super.addToken(tokenId);
-                        }
-                    }
-
-                    protected void onTokenClicked(final Object tokenId) {
-                        final TokenField field = this;
-                        final Window w = new Window("Are you sure?");
-                        w.setStyleName("light");
-                        w.setResizable(false);
-                        w.center();
-                        w.setModal(true);
-                        getWindow().addWindow(w);
-
-                        Button remove = new Button("Remove "
-                                + field.getTokenCaption(tokenId),
-                                new Button.ClickListener() {
-                                    public void buttonClick(ClickEvent event) {
-                                        field.removeToken(tokenId);
-                                        DemoWindow.this.removeWindow(w);
-                                    }
-                                });
-                        w.addComponent(remove);
-
-                        Button cancel = new Button("Cancel",
-                                new Button.ClickListener() {
-                                    public void buttonClick(ClickEvent event) {
-                                        DemoWindow.this.removeWindow(w);
-                                    }
-                                });
-                        cancel.setStyleName(Button.STYLE_LINK);
-                        w.addComponent(cancel);
-
-                    }
-                };
-                f.setNewTokensAllowed(true);
-                p.addComponent(f);
             }
 
             {
@@ -296,12 +216,13 @@ public class TokenfieldDemo extends Application {
 
                 // just for layout; ListSelect left, TokenField right
                 HorizontalLayout lo = new HorizontalLayout();
+                lo.setWidth("100%");
                 lo.setSpacing(true);
                 p.setContent(lo);
 
                 // A regular list select
                 ListSelect list = new ListSelect();
-                p.addComponent(list);
+                lo.addComponent(list);
                 list.setImmediate(true);
                 list.setMultiSelect(true);
                 list.setContainerDataSource(tokens);
@@ -313,8 +234,111 @@ public class TokenfieldDemo extends Application {
                 // f.setNewTokensAllowed(false);
                 f.setFilteringMode(ComboBox.FILTERINGMODE_CONTAINS);
                 f.setPropertyDataSource(list);
-                p.addComponent(f);
+                lo.addComponent(f);
+                lo.setExpandRatio(f, 1.0f);
             }
+        }
+    }
+
+    /**
+     * This is the window used to confirm removal
+     */
+    class RemoveWindow extends Window {
+        RemoveWindow(final Contact c, final TokenField f) {
+            super("Remove " + c.getName() + "?");
+
+            setStyleName("black");
+            setResizable(false);
+            center();
+            setModal(true);
+            setWidth("250px");
+            setClosable(false);
+
+            // layout buttons horizontally
+            HorizontalLayout hz = new HorizontalLayout();
+            addComponent(hz);
+            hz.setSpacing(true);
+            hz.setWidth("100%");
+
+            Button cancel = new Button("Cancel", new Button.ClickListener() {
+                public void buttonClick(ClickEvent event) {
+                    f.getWindow().removeWindow(getWindow());
+                }
+            });
+            hz.addComponent(cancel);
+            hz.setComponentAlignment(cancel, Alignment.MIDDLE_LEFT);
+
+            Button remove = new Button("Remove", new Button.ClickListener() {
+                public void buttonClick(ClickEvent event) {
+                    f.removeToken(c);
+                    f.getWindow().removeWindow(getWindow());
+                }
+            });
+            hz.addComponent(remove);
+            hz.setComponentAlignment(remove, Alignment.MIDDLE_RIGHT);
+
+        }
+    }
+
+    /**
+     * This is the window used to add new contacts to the 'address book'.
+     * It does not do proper validation - you can add weird stuff.
+     */
+    class EditContactWindow extends Window {
+        private Contact contact;
+
+        EditContactWindow(final String t, final TokenField f) {
+            super("New Contact");
+            if (t.contains("@")) {
+                contact = new Contact("", t);
+            } else {
+                contact = new Contact(t, "");
+            }
+            setModal(true);
+            center();
+            setWidth("250px");
+            setStyleName("black");
+            setResizable(false);
+
+            // Just bind a Form to the Contact -pojo via BeanItem
+            Form form = new Form();
+            form.setItemDataSource(new BeanItem(contact));
+            form.setImmediate(true);
+            addComponent(form);
+
+            // layout buttons horizontally
+            HorizontalLayout hz = new HorizontalLayout();
+            addComponent(hz);
+            hz.setSpacing(true);
+            hz.setWidth("100%");
+
+            Button dont = new Button("Don't add", new Button.ClickListener() {
+                public void buttonClick(ClickEvent event) {
+                    if (contact.getEmail()==null||contact.getEmail().length()<1) {
+                        contact.setEmail(contact.getName());
+                    }
+                    f.addToken(contact);
+                    f.getWindow().removeWindow(getWindow());
+                }
+            });
+            hz.addComponent(dont);
+            hz.setComponentAlignment(dont, Alignment.MIDDLE_LEFT);
+
+            Button add = new Button("Add to contacts",
+                    new Button.ClickListener() {
+                        public void buttonClick(ClickEvent event) {
+                            if (contact.getEmail()==null||contact.getEmail().length()<1) {
+                                contact.setEmail(contact.getName());
+                            }
+                            ((BeanItemContainer) f.getContainerDataSource())
+                                    .addBean(contact);
+                            f.addToken(contact);
+                            f.getWindow().removeWindow(getWindow());
+                        }
+                    });
+            hz.addComponent(add);
+            hz.setComponentAlignment(add, Alignment.MIDDLE_RIGHT);
+
         }
     }
 
@@ -329,24 +353,72 @@ public class TokenfieldDemo extends Application {
             "Fielding", "Einstein" };
 
     private Container generateTestContainer() {
+        BeanItemContainer<Contact> container = new BeanItemContainer<Contact>(
+                Contact.class);
 
-        IndexedContainer tokens = new IndexedContainer();
-        tokens.addContainerProperty("name", String.class, null);
-
+        HashSet<String> log = new HashSet<String>();
         Random r = new Random(5);
-        for (int i = 0; i < 50;) {
+        for (int i = 0; i < 20;) {
             String fn = firstnames[(int) (r.nextDouble() * firstnames.length)];
             String ln = lastnames[(int) (r.nextDouble() * lastnames.length)];
             String name = fn + " " + ln;
             String email = fn.toLowerCase() + "." + ln.toLowerCase()
                     + "@example.com";
-            Item item = tokens.addItem(email);
-            if (item != null) {
+
+            if (!log.contains(email)) {
+                log.add(email);
+                container.addBean(new Contact(name, email));
                 i++;
-                item.getItemProperty("name").setValue(name);
             }
+
         }
-        return tokens;
+        return container;
     }
 
+    /**
+     * Example Contact -bean, mostly generated setters/getters.
+     */
+    public class Contact {
+        private String name;
+        private String email;
+
+        public Contact(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String toString() {
+            return email;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Contact) {
+                return email.equals(((Contact) obj).getEmail());
+            }
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return email.hashCode();
+        }
+
+    }
 }
